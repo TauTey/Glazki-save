38b166cd77ef327eb469526e5feabc63991362c1
PopovDima <legdim17@gmail.com>
2026-02-18 12:42:40 +0530

    17

--- AgentPage.xaml.cs
+++ AgentPage.xaml.cs
@@ -23,11 +23,80 @@ namespace Козин_Глазки_save
         public AgentPage()
         {
             InitializeComponent();
+            var currentAgents = KozinEntities.GetContext().Agent.ToList();
+            AgentListView.ItemsSource = currentAgents;
+
+            ComboSorting.SelectedIndex = 0;
+            ComboType.SelectedIndex = 0;
+
+            UpdateAgents();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void UpdateAgents()
         {
-            Manager.MainFrame.Navigate(new AddEditPage());
+            var context = KozinEntities.GetContext();
+            var currentAgents = context.Agent.ToList();
+
+            // Поиск по тексту (в названии или телефоне)
+            if (!string.IsNullOrWhiteSpace(TBoxSearch.Text))
+            {
+                var searchText = TBoxSearch.Text.ToLower();
+                currentAgents = currentAgents.Where(a =>
+                    (a.Title.ToLower().Contains(TBoxSearch.Text.ToLower())) ||
+                    (a.Phone.ToLower().Contains(TBoxSearch.Text.ToLower())) ||
+                    (a.Email.ToLower().Contains(TBoxSearch.Text.ToLower())) ||
+                    (a.currentnumber.ToLower().Contains(TBoxSearch.Text.ToLower()))
+                ).ToList();
+            }
+
+            // ComboType
+            if (ComboType.SelectedIndex > 0) // 0 - "Все типы"
+            {
+                var selectedType = (ComboType.SelectedItem as TextBlock)?.Text;
+                if (!string.IsNullOrEmpty(selectedType))
+                {
+                    currentAgents = currentAgents.Where(a => a.AgentTypeName == selectedType).ToList();
+                }
+            }
+
+            // ComboSorting
+            switch (ComboSorting.SelectedIndex)
+            {
+                case 1: // Наименование от А до Я
+                    currentAgents = currentAgents.OrderBy(a => a.Title).ToList();
+                    break;
+                case 2: // Наименование от Я до А
+                    currentAgents = currentAgents.OrderByDescending(a => a.Title).ToList();
+                    break;
+                case 3: // Скидка по возрастанию
+                    currentAgents = currentAgents.OrderBy(a => a.Discount).ToList();
+                    break;
+                case 4: // Скидка по убыванию
+                    currentAgents = currentAgents.OrderByDescending(a => a.Discount).ToList();
+                    break;
+                case 5: // Приоритет по возрастанию
+                    currentAgents = currentAgents.OrderBy(a => a.Priority).ToList();
+                    break;
+                case 6: // Приоритет по убыванию
+                    currentAgents = currentAgents.OrderByDescending(a => a.Priority).ToList();
+                    break;
+            }
+            AgentListView.ItemsSource = currentAgents;
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateAgents();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAgents();
+        }
+
+        private void ComboSorting_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAgents();
         }
     }
 }
